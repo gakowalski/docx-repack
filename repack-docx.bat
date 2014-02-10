d798dba33ee89d83cec7050ac8d552cd671d554a
g_a_kowalski <grzegorz.kowalski@wit.edu.pl>
2014-02-10 14:40:50 +0100

    Added support for XLSX, PPTX, ODT. Added checks for valid paths. Added internal configuration option for making backup copy.

--- repack-docx.bat
+++ repack-docx.bat
@@ -14,10 +14,89 @@ set JPG_OPTIMIZER=c:\IT\jpegtran.exe
 rem Folder name with leading backslash
 set TEMP_SUBFOLDER=\Docx-Repack
 
+rem Some default options
+set CONFIG_MAKE_BACKUP=Yes
+
+if not exist %1 goto label_no_input
+
+:decompress
+if not exist "%COMPRESSOR%" goto label_no_compressor
 "%COMPRESSOR%" x -y -tzip %1 -o%TEMP%%TEMP_SUBFOLDER%
-move %1 %1.bak
-%PNG_OPTIMIZER% --preserve -o5 %TEMP%%TEMP_SUBFOLDER%\word\media\*.png 
-FOR /F %%j IN ('dir /B %TEMP%%TEMP_SUBFOLDER%\word\media\*.jpeg') DO %JPG_OPTIMIZER% -optimize %TEMP%%TEMP_SUBFOLDER%\word\media\%%j %TEMP%%TEMP_SUBFOLDER%\word\media\%%j
-FOR /F %%j IN ('dir /B %TEMP%%TEMP_SUBFOLDER%\word\media\*.jpg') DO %JPG_OPTIMIZER% -optimize %TEMP%%TEMP_SUBFOLDER%\word\media\%%j %TEMP%%TEMP_SUBFOLDER%\word\media\%%j
+
+:make_backup
+if "%CONFIG_MAKE_BACKUP%"=="Yes" move %1 %1.bak
+
+:optimize_png
+if not exist "%PNG_OPTIMIZER%" goto optimize_jpeg
+
+rem Media folder for DOCX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\word\media
+if exist "%MEDIA_FOLDER%" (
+"%PNG_OPTIMIZER%" --preserve -o5 %MEDIA_FOLDER%\*.png 
+)
+
+rem Media folder for PPTX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\ppt\media
+if exist "%MEDIA_FOLDER%" (
+"%PNG_OPTIMIZER%" --preserve -o5 %MEDIA_FOLDER%\*.png 
+)
+
+rem Media folder for XLSX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\xl\media
+if exist "%MEDIA_FOLDER%" (
+"%PNG_OPTIMIZER%" --preserve -o5 %MEDIA_FOLDER%\*.png 
+)
+
+rem Media folder for ODT files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\media
+if exist "%MEDIA_FOLDER%" (
+"%PNG_OPTIMIZER%" --preserve -o5 %MEDIA_FOLDER%\*.png 
+)
+
+:optimize_jpeg
+if not exist "%JPG_OPTIMIZER%" goto compress
+
+rem Media folder for DOCX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\word\media
+if exist "%MEDIA_FOLDER%" (
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpeg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+)
+
+rem Media folder for PPTX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\ppt\media
+if exist "%MEDIA_FOLDER%" (
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpeg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+)
+
+rem Media folder for XLSX files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\xl\media
+if exist "%MEDIA_FOLDER%" (
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpeg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+)
+
+rem Media folder for ODT files
+set MEDIA_FOLDER=%TEMP%%TEMP_SUBFOLDER%\media
+if exist "%MEDIA_FOLDER%" (
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpeg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+FOR /F %%j IN ('dir /B %MEDIA_FOLDER%\*.jpg') DO %JPG_OPTIMIZER% -optimize %MEDIA_FOLDER%\%%j %MEDIA_FOLDER%\%%j
+)
+
+:compress
 "%COMPRESSOR%" a -tzip %1 -r %TEMP%%TEMP_SUBFOLDER%\* -mx=9 -mfb=258 -mm=Deflate -mpass=15
+
+:cleanup
 rmdir /S /Q %TEMP%%TEMP_SUBFOLDER%
+goto label_exit
+
+:label_no_input
+echo File "%1" does not exist!
+goto label_exit
+
+:label_no_compressor
+echo No archiving utility at this path: "%COMPRESSOR%"!
+goto label_exit
+
+:label_exit
